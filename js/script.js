8e257e24fb484f9adabf69cba6db548ebf25c2c7
Vica Fresinsya <vicafresinsya@gmail.com>
2022-10-22 14:40:55 +0700

    utsPemWeb

---
+++ js/script.js
@@ -0,0 +1,23 @@
+// init Isotope
+let $grid = $('.collection-list').isotope({
+    // options
+});
+// filter items on button click
+$('.filter-button-group').on('click', 'button', function () {
+    let filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
+    $grid.isotope({ filter: filterValue });
+});
+
+let filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns() {
+    filterBtns.each(function () {
+        $(this).removeClass('active-filter-btn');
+    });
+}
+
+$('#searchForm').on('shown.bs.collapse', function () {
+    // focus input on collapse
+    $("#search").focus()
+})
